10c8db9e7775e575af9414799fe0be8133178e15
wahidanik <wahid.anik@outlook.com>
2023-06-08 15:54:09 +0600

    verifyJTW middleware added

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 app.use(express.json());
 app.use(cors())
 
+// verify token 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    //bearer token 
+    const token = authorization.split(' ')[1]
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+        if (error) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 //----------------- Mongo DB ------------------------
 const client = new MongoClient(uri, {
     serverApi: {
@@ -34,12 +52,18 @@ async function run() {
         // user Database table
         const users = database.collection("users");
 
+        app.post('/jwt', (req, res) => {
+            const userEmail = req.body;
+            const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN, { expiresIn: '24h' });
+            res.send({ token })
+        })
+
         // set user to database 
-        app.post('/setuser', async(req, res) => {
+        app.post('/setuser', async (req, res) => {
             const user = req.body;
-            const query = {email: user.email};
+            const query = { email: user.email };
             const find = await users.findOne(query);
-            if(find){
+            if (find) {
                 return;
             }
             const result = await users.insertOne(user);
